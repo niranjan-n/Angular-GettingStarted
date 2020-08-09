bde26a7ec1b898c994a7a5e5e53fc79ec9e18a20
niranjan-n <hegdeniranjan97@gmail.com>
2020-08-09 22:49:33 +0530

    binding to directive properties

--- src/app/better-directive/better-directive.directive.ts
+++ src/app/better-directive/better-directive.directive.ts
@@ -1,21 +1,25 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterDirective]'
 })
 export class BetterDirectiveDirective implements OnInit{
-
+  @Input() defaultColor='transparent';
+  @Input() highlightColor='green';
+  @HostBinding('style.backgroundColor') backgroundColor=this.defaultColor;
   constructor(private eleRef:ElementRef,private renderer:Renderer2) { 
   }
   ngOnInit(){
+    this.backgroundColor=this.defaultColor;
   //  this.renderer.setStyle(this.eleRef.nativeElement,'background-color','orange');
   }
   @HostListener('mouseenter') mouseEnter(eventData:Event){
-     this.renderer.setStyle(this.eleRef.nativeElement,'background-color','orange');
-
+     //this.renderer.setStyle(this.eleRef.nativeElement,'background-color','orange');
+    //this.backgroundColor='green';
+    this.backgroundColor=this.highlightColor;
   }
   @HostListener('mouseleave') mouseLeave(eventData:Event){
-      this.renderer.setStyle(this.eleRef.nativeElement,'background-color','transparent');
-
+     // this.renderer.setStyle(this.eleRef.nativeElement,'background-color','transparent');
+     this.backgroundColor=this.defaultColor;
   }
 }
